a5580b8245169ed1571af061b3f668f310c1a1c2
Luc Succes <luc@onoctober.com>
2015-08-13 14:34:12 -0400

    Removes build folder

--- build/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-var BaseURL = 'https://asterix.firebaseio.com';
-exports.BaseURL = BaseURL;
-//# sourceMappingURL=config.js.map
