02bc12b158c26c7c3c2405f1b2b8809b1672d898
Frank J Pinto <frank.joseph.pinto@gmail.com>
2015-10-30 19:07:50 -0400

    Re-indent UserWorker with tabs

--- app/Workers/UserWorker.js
+++ app/Workers/UserWorker.js
@@ -7,34 +7,34 @@ import _ from 'underscore';
 
 class UserWorker extends Worker {
 
-  constructor (opts = {}) {
-    let options = _.defaults(opts, FirebaseConfig.queues.user);
-    super(options);
-  }
+	constructor (opts = {}) {
+		let options = _.defaults(opts, FirebaseConfig.queues.user);
+		super(options);
+	}
 
-  /**
-   * Processes the user related tasks
-   * @param {object} data - data object from the user queue
-   *  Required attributes:
-   *	user - id of user on whose behalf a given task is to be processed
-   *  task - specifies the operation to be performed. Currently supported tasks are: addFavorite, removeFavorite, addRecent
-   *  result - contains a search result on which the task should be performed
-   *  Example:
-   *
-   *  data : {
-   *		user : "myUser123",
-   *		task : "addFavorites",
-   *		result : {
-   *			_id : someGuid,
-   *			//more stuff here
-   *		}
-   *	}
-   * @param {function} progress - callback function for reporting the state of an item
-   * @param {function} resolve - callback marking the task as complete
-   * @param {function} reject - callback marking the task as incomplete
-   *
-   * @return {void}
-   */
+	/**
+	 * Processes the user related tasks
+	 * @param {object} data - data object from the user queue
+	 *  Required attributes:
+	 *	user - id of user on whose behalf a given task is to be processed
+	 *  task - specifies the operation to be performed. Currently supported tasks are: addFavorite, removeFavorite, addRecent
+	 *  result - contains a search result on which the task should be performed
+	 *  Example:
+	 *
+	 *  data : {
+	 *		user : "myUser123",
+	 *		task : "addFavorites",
+	 *		result : {
+	 *			_id : someGuid,
+	 *			//more stuff here
+	 *		}
+	 *	}
+	 * @param {function} progress - callback function for reporting the state of an item
+	 * @param {function} resolve - callback marking the task as complete
+	 * @param {function} reject - callback marking the task as incomplete
+	 *
+	 * @return {void}
+	 */
 	process (data, progress, resolve, reject) {
 		try {
 			if (data.task == 'addFavorite') {
