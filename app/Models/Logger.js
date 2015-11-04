f9874b0c0bff32a2a2a7221a6493b130a33f3fb8
Jakub Cichon <jakub@drizzyapp.com>
2015-11-04 17:51:22 -0500

    Implemented a distributed logger via winston

---
+++ app/Models/Logger.js
@@ -0,0 +1,37 @@
+import winston from 'winston';
+import { GCL } from 'winston-gcl';
+import { Firebase } from 'winston-firebase';
+import config from '../config';
+import { firebase as FirebaseConfig } from '../config';
+import { winston as WinstonConfig } from '../config'
+import _ from 'underscore';
+import google from 'googleapis';
+import os from 'os';
+
+winston.transports.GCL = GCL;
+
+var logger = new winston.Logger(WinstonConfig.global);
+
+for (var transport of WinstonConfig.transports) {
+
+	var transportDetails = transport.details;
+	if (transport.type == 'GCL') {
+		var jwtClient = new google.auth.JWT(null, null, null, transport.gcloud_endpoint);
+		jwtClient.fromJSON(transport.credentials);
+		transportDetails.auth = jwtClient;
+	} 
+	logger.add( winston.transports[transport.type], transportDetails );
+}
+
+logger.rewriters.push( (level, msg, meta) => {
+	var newMeta = {};
+	newMeta.date = new Date();
+	newMeta.host = os.hostname();
+	newMeta.env = FirebaseConfig.BaseURL;
+	newMeta.worker = config.worker;
+	newMeta.data = meta;
+
+	return newMeta;
+});
+
+export default logger;
--- app/app.js
+++ app/app.js
@@ -10,24 +10,28 @@ var argv = minimist(process.argv.slice(2));
 console.dir(argv);
 
 if (_.contains(argv._, 'search')) {
+	config.worker = 'search';
 	console.log('starting search\n');
 	var worker = new SearchWorker();
 	worker.start();
 }
 
 if (_.contains(argv._, 'feed-ingestor')) {
+	config.worker = 'feed-ingestor';
 	console.log('starting feed ingestor\n');
 	var worker = new FeedIngestorWorker();
 	worker.start();
 }
 
 if (_.contains(argv._, 'feedpage-parser')) {
+	config.worker = 'feedpage-parser';
 	console.log('starting feed page parser');
 	var worker = new FeedPageWorker();
 	worker.start();
 }
 
 if (_.contains(argv._, 'user')) {
+	config.worker = 'user';
 	console.log('starting the user worker');
 	var worker = new UserWorker();
 	worker.start();
