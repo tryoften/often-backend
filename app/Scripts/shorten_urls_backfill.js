7171f515a430ec86ac289fa6d35a86f9f5b76607
Luc Succes <luc@onoctober.com>
2015-12-01 13:26:47 -0500

    Updates URL backfill script

--- app/Scripts/shorten_urls_backfill.js
+++ app/Scripts/shorten_urls_backfill.js
@@ -1,38 +1,53 @@
 import { Client } from 'elasticsearch';
 import { elasticsearch as ElasticSearchConfig } from '../config';
 import URLHelper from '../Utilities/URLHelper';
+import minimist from 'minimist';
 
 var client = new Client({
 	host: ElasticSearchConfig.BaseURL
 });
 var urlHelper = new URLHelper();
+var argv = minimist(process.argv.slice(2));
+
+var indexes = [	
+	"billboard", 
+	"complex-music", 
+	"elitedaily", 
+	"factmag", 
+	"fader", 
+	"fourpins", 
+	"highsnobiety",
+	"hnhh",
+	"hypebeast",
+	"mtv-news",
+	"paper",
+	"pigeonsandplanes",
+	"spin-music",
+	"tmz",
+	"vibe-music",
+	"vibe-news",
+	"vice",
+	"xxlmag" 
+];
+
+if (argv._.length > 0) {
+	indexes = argv._;
+	console.log("Processing indexes: ", indexes);
+}
 
 client.search({
-  index: [	"billboard", 
-  			"complex-music", 
-  			"elitedaily", 
-  			"factmag", 
-  			"fader", 
-  			"fourpins", 
-  			"highsnobiety",
-  			"hnhh",
-  			"hypebeast",
-  			"mtv-news",
-  			"paper",
-  			"pigeonsandplanes",
-  			"spin-music",
-  			"tmz",
-  			"vibe-music",
-  			"vibe-news",
-  			"vice",
-  			"xxlmag" ],
+  index: indexes,
   q: '*:*',
-  size: 100000 // a number higher than total number of documents in listed indices
+  size: 200000 // a number higher than total number of documents in listed indices
 }, (error, response) => {
 
 	var documents = response.hits.hits;
 	var bulkBody = [];
+	var i = 0;
+	console.warn(documents.length + " documents to convert");
+
 	for (var doc of documents) {
+		console.log("converting document #" + (i++) + ": " + doc._id);
 		bulkBody.push({
 			update: {
 				_index: doc._index, 
@@ -44,7 +59,9 @@ client.search({
 		var updObj = {
 			original_url: doc._id,
 			link: urlHelper.shortenUrl(doc._id)
-		}
+		};
+
+		// console.log("shortened url: ", updObj.link);
 
 		bulkBody.push({
 			doc : updObj
@@ -54,6 +71,12 @@ client.search({
 	/* Bulk update all documents */
 	client.bulk({
 		body : bulkBody
+	}, (err, res) => {
+		if (err) {
+			process.exit(1);
+			return;
+		}
+		process.exit();
 	});
   	
 });
