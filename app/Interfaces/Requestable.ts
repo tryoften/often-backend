d63436ee7df11f402586919df173f73cd6a8949a
Jakub Cichon <jakub@drizzyapp.com>
2016-01-13 17:14:53 -0500

    Added requestable interface

---
+++ app/Interfaces/Requestable.ts
@@ -0,0 +1,11 @@
+import {QueryData} from "./Queryable";
+import RequestType from "../Models/RequestType";
+import MediaItemSource from "../Models/MediaItemSource";
+export interface Requestable {
+	id: string,
+	userId: string,
+	creation_time?: number,
+	query: QueryData,
+	type: RequestType
+	filters?: MediaItemSource[]
+}
