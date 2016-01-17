1c8212d627eb912252ff8ac37add5d6c0277132c
Jakub Cichon <jakub@drizzyapp.com>
2016-01-16 20:58:30 -0500

    Modified ingestion script

--- app/Scripts/IngestionScript.ts
+++ app/Scripts/IngestionScript.ts
@@ -9,32 +9,17 @@ var gs = new GeniusService({
 var search = new Search();
 //21 - 24
 pi.ingestPopularTracks().then((tracks) => {
-	console.log(tracks.length);
-	var interval = 10;
-	var forElastic = [];
-	function throttleIngest(st, en) {
+	var p = new Promise((resolve, reject) => {
+		resolve(true);
+	});
+	var count = 0;
+	tracks.forEach((trackId) => {
+		console.log("Count: "+ count + " trackId: "+ trackId);
+		count++;
+		p = p.then(function(){
+			return gs.ingest([trackId]);
+		});
+	});
 
-		var subset = tracks.slice(st, en);
-		gs.ingest(subset).then( res => {
-			console.log(`Ingesting from: ${st} to ${en}`);
-			forElastic = forElastic.concat(res);
-			if (en >= tracks.length) {
-				doIngest(forElastic);
-				return;
-			};
-			throttleIngest(en, ( (en + interval) < tracks.length) ? (en + interval) : tracks.length);
-		});
-	}
-	throttleIngest(0, interval);
-	function doIngest (arr) {
-		console.log('About to index...');
-		search.index(arr).then(() => {
-			console.log('done!');
-		});
-	}
 });
 
-
-/**
- * Created by jdc on 1/15/16.
- */
