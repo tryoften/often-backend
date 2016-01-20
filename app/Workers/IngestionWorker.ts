f628ee10e684b93e6253dfdbccddc11be7458622
Jakub Cichon <jakub@drizzyapp.com>
2016-01-20 17:21:50 -0500

    Ingestion Worker changes

--- app/Workers/IngestionWorker.ts
+++ app/Workers/IngestionWorker.ts
@@ -2,25 +2,32 @@ import Worker from './Worker';
 import { firebase as FirebaseConfig } from '../config';
 import GeniusService from '../Services/Genius/GeniusService';
 import * as _ from 'underscore';
+import Search from '../Search/Search';
 
 class IngestionWorker extends Worker {
 	genius: GeniusService;
+	search: Search;
 
 	constructor (opts = {}) {
+		console.log('initiating');
 		let options = _.defaults(opts, FirebaseConfig.queues.track_ingestion);
 		super(options);
 		this.genius = new GeniusService({
 			provider_id: 'genius'
 		});
-		console.log('done');
+		this.search = new Search();
 	}
 
 	process (data, progress, resolve, reject) {
 		console.log("Processing: " + Object.keys(data));
 		// returns a promise when all providers are resolved
-		return this.genius.ingest([data.trackId])
-			.then(d => {
-				resolve(d);
+		return this.genius.ingest(data.tracks)
+			.then(indexableData => {
+				/* Ingest data to ElasticSearch */
+				return this.search.index(indexableData);
+			})
+			.then((response) => {
+				resolve(response);
 			})
 			.catch(err => {
 				reject(err);
