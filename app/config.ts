a8e8185fbdde1675df74b3411e5ac3c807d3ee98
Luc Succes <luc@onoctober.com>
2016-02-26 14:39:53 -0500

    Fixes logger bug

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,13 @@
 const config = require('config');
 
+var args = require('minimist')(process.argv.slice(2));
+config.worker = (function() {
+	if (args._.length > 0) {
+			return args._[0];
+		}
+	return 'not-set';
+})();
+
 export default config;
 export var trackSchedulerOptions = config.trackSchedulerOptions;
 export var firebase = config.firebase;
