3d59f58f636c1abb2017d438d4bb8683b212318d
Jakub Cichon <jakubcichon@Jakubs-MacBook-Pro.local>
2016-02-26 16:08:04 -0500

    More bug fixes

--- app/logger.ts
+++ app/logger.ts
@@ -1,3 +1,4 @@
+
 import * as winston from 'winston';
 import { GCL } from 'winston-gcl';
 import config from './config';
@@ -10,29 +11,30 @@ winston.transports.GCL = GCL;
 
 var logger = new winston.Logger(WinstonConfig.global);
 
-	for (var transport of WinstonConfig.transports) {
-		var transportDetails = transport.details;
-		if (transport.type === 'GCL') {
-			var jwtClient = new google.auth.JWT(null, null, null, transport.gcloud_endpoint);
-			jwtClient.fromJSON(transport.credentials);
-			transportDetails.logId = config.workers[0];
-			transportDetails.googleMetadata.id = os.hostname();
-			transportDetails.auth = jwtClient;
-		}
-		if (_.has(winston.transports, transport.type)) {
-			logger.add(winston.transports[transport.type], transportDetails);
-		}
+for (var transport of WinstonConfig.transports) {
+	var transportDetails = transport.details;
+	if (transport.type === 'GCL') {
+		var jwtClient = new google.auth.JWT(null, null, null, transport.gcloud_endpoint);
+		jwtClient.fromJSON(transport.credentials);
+		transportDetails.logId = config.worker;
+		transportDetails.googleMetadata.id = os.hostname();
+		transportDetails.auth = jwtClient;
 	}
+	if (_.has(winston.transports, transport.type)) {
+		logger.add(winston.transports[transport.type], transportDetails);
+	}
+}
 
-	logger.rewriters.push( (level, msg, meta) => {
-		return {
-			date: new Date(),
-			host: os.hostname(),
-			env: FirebaseConfig.BaseURL,
-			workers: config.workers,
-			data: meta
-		};
-	});
+logger.rewriters.push((level, msg, meta) => {
+	return {
+		date: new Date(),
+		host: os.hostname(),
+		env: FirebaseConfig.BaseURL,
+		workers: config.workers,
+		data: meta
+	};
+
+});
 
 
 export default logger;
