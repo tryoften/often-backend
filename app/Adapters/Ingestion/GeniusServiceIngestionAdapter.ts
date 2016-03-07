fc337cc535fbbd8f78a47933c333eaeb1bc34221
Jakub Cichon <jakubcichon@Jakubs-MacBook-Pro.local>
2016-03-06 23:59:21 -0500

    Removed code comment that contained the logic of dumping indexable objects directly to disk

--- app/Adapters/Ingestion/GeniusServiceIngestionAdapter.ts
+++ app/Adapters/Ingestion/GeniusServiceIngestionAdapter.ts
@@ -501,7 +501,7 @@ class GeniusServiceIngestionAdapter extends IngestionAdapter {
 
 
 	/**
-	 * TODO(jakub): Fill in remaining parameters and test
+	 * TODO(jakub): Fill in remaining parameters and test thoroughly
 	 * Recursively fetches track ids by arsing artist pages
 	 *
 	 * @param {TrackId[]} trackIds - Array of track ids to be populated through recursive parsing of pages (defaults to [])
@@ -538,86 +538,3 @@ class GeniusServiceIngestionAdapter extends IngestionAdapter {
 
 
 export default GeniusServiceIngestionAdapter;
-
-/*
-process(data: GeniusServiceIngestionRequest, progress, resolve, reject)
-{
-	logger.info('GeniusServiceIngestionWorker.process(): owner-id: ', data._owner, ' ids: ', data.ids, ' type: ', data.type);
-
-	var ids = data.ids;
-	var type = data.type;
-	var targets = data.targets;
-
-	var MediaItemClass = MediaItemType.toClass(type);
-	var promises = [];
-
-	for (var id of ids) {
-		var mediaItem = new MediaItemClass({id: id});
-		promises.push(mediaItem.syncData());
-	}
-
-	Promise.all(promises).then(syncedMediaItems => {
-		logger.info('GeniusServiceIngestionWorker.process(): owner-id: ', data._owner, ' ids: ', data.ids, ' event: Synced all data.');
-		var indexables = [];
-		for (var smi of syncedMediaItems) {
-			var indexingFormat = smi.toIndexingFormat();
-			indexables.push(indexingFormat);
-		}
-		return indexables;
-	}).then(allIndexables => {
-
-		let targetPromises = [];
-		for (let target of targets) {
-			switch (target.type) {
-				case (IngestionTargetType.file):
-					targetPromises.push(this.writeToFS(target.data, allIndexables, type));
-					break;
-
-				case (IngestionTargetType.elasticsearch):
-					targetPromises.push(this.indexToES(allIndexables));
-					break;
-
-				default:
-					reject(targetPromises);
-					return;
-			}
-		}
-		return Promise.all(targetPromises);
-
-	}).then((results) => {
-		logger.info('GeniusServiceIngestionWorker.process(): owner-id: ', data._owner,
-			' ids: ', data.ids, ' type: ', data.type, ' results: ', results);
-		resolve(results);
-	}).catch(err => {
-		logger.error('GeniusServiceIngestionWorker.process(): owner-id: ', data._owner,
-			' ids: ', data.ids, ' type: ', data.type, ' error: ', err);
-		reject(err);
-	});
-}
-
-indexToES(indexables: IndexableObject[]){
-	return this.search.index(indexables);
-}
-
-writeToFS(dir: string, indexables: IndexableObject[], type:MediaItemType)
-{
-
-	return new Promise((resolve, reject) => {
-		var writeString = '';
-		for (var index of indexables) {
-			var esFormat = this.search.getIndexFormat(index);
-			writeString += JSON.stringify(esFormat[0]) + '\n' + JSON.stringify((esFormat[1])) + '\n';
-		}
-
-		var fileName = `${dir}/${type}-${Date.now()}`;
-		fs.writeFile(fileName, writeString, (err) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(fileName);
-			}
-		});
-	});
- }
-
-*/
