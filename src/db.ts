4ced2f426b12c2c68bdbc675925d0254c0f4e1d3
Luc Succes <l2succes@gmail.com>
2016-07-26 14:00:51 -0400

    Fixes db error

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,11 @@ import { firebase as FirebaseConfig } from './config';
 
 const firebase = require('firebase');
 
-var main = firebase.initializeApp(FirebaseConfig.credentials);
-console.log("Initialized Firebase (main)", FirebaseConfig.credentials);
+try {
+	var main = firebase.initializeApp(FirebaseConfig.credentials);
+	console.log("Initialized Firebase (main)", FirebaseConfig.credentials);
+} catch (e) {
+	console.log("Database already initialized", e);
+}
 
 export default main;
