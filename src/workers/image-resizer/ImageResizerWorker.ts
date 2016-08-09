886be930c5da6976e3762d2b81beddd5e7adfb24
Jakub Cichon <jakub@tryoften.com>
2016-08-09 15:44:17 -0400

    Restored correct options

--- src/workers/image-resizer/ImageResizerWorker.ts
+++ src/workers/image-resizer/ImageResizerWorker.ts
@@ -26,8 +26,6 @@ class ImageResizerWorker extends Worker {
 	private maxCacheExpiry: number;
 
 	constructor (opts = {}) {
-
-		FirebaseConfig.queues.image_resizing.url = FirebaseConfig.BaseURL + FirebaseConfig.queues.image_resizing.url;
 		let options = _.defaults(opts, FirebaseConfig.queues.image_resizing);
 		super(options);
 
